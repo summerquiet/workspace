342eae494eb0c177dc30cc54947eaa6a6001bda0
guoxianing <guoxianing@pset.suntec.net>
2020-05-20 16:01:21 +0800

    add CmakeProjTest in vs2019

---
+++ CMakeProjTest/src/CMakeProjTest.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjTest.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
