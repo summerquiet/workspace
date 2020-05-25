bed7758fa1289fa93db21f4ecac0607f7ef2228c
summerquiet <summerquiet@gmail.com>
2020-05-25 10:03:57 +0800

    add some codes

---
+++ codes/nmea_check_sum.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int checksum(const char *s)
+{
+    int c = 0;
+    while (*s) {
+        c ^= *s++;
+    }
+    return c;
+}
+
+int main()
+{
+    char mystring[] = "BNALF,1,1,0,,B,W,V,,220003,,0,0,VISUAL ALARM";
+    printf("String: %s\nChecksum: 0x%02X\n", mystring, checksum(mystring));
+    return 0;
+}
---
+++ codes/simple_code.c
@@ -0,0 +1,40 @@
+#define _ -F<OO||--F-OO--;
+#define X --F<OO||--F-OO--;
+int F=00, OO=00;main(){F_OO();printf("%1.3f, %d, %d\n", 4.*-F/OO/OO, F, OO);}F_OO()
+{
+#if 0
+                _-_-_-_
+           _-_-_-_-_-_-_-_-_
+        _-_-_-_-_-_-_-_-_-_-_-_
+      _-_-_-_-_-_-_-_-_-_-_-_-_-_
+     _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+     _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+     _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+     _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+      _-_-_-_-_-_-_-_-_-_-_-_-_-_
+        _-_-_-_-_-_-_-_-_-_-_-_
+            _-_-_-_-_-_-_-_
+                _-_-_-_
+#else
+                _ X X X
+           _ X X X X X X X
+        _ X X X X X X X X X X X
+      _ X X X X X X X X X X X X X
+     _ X X X X X X X X X X X X X X
+     _ X X X X X X X X X X X X X X
+    _ X X X X X X X X X X X X X X X
+    _ X X X X X X X X X X X X X X X
+    _ X X X X X X X X X X X X X X X
+    _ X X X X X X X X X X X X X X X
+     _ X X X X X X X X X X X X X X
+     _ X X X X X X X X X X X X X X
+      _ X X X X X X X X X X X X X
+        _ X X X X X X X X X X X
+            _ X X X X X X X
+                _ X X X
+#endif
+}
